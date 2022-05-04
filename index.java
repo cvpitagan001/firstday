cb372cdcfb09f5df112a4abfea15fafaf28ce386
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-05-04 12:05:18 +0800

    changed content

--- index.java
+++ index.java
@@ -1,11 +1,6 @@
 public class index {
     public static void main(String[] args) {
         // System.out.format("The final game score: %-8s %8s", "10", "5");
-        System.out.format("%8s %8s %8s %8s %8s", "B", "I", "N", "G", "O\n");
-        System.out.format("%8s %8s %8s %8s %8s", "2", "20", "42", "60", "64\n");
-        System.out.format("%8s %8s %8s %8s %8s", "2", "20", "42", "60", "64\n");
-        System.out.format("%8s %8s %8s %8s %8s", "2", "20", "42", "60", "64\n");
-        System.out.format("%8s %8s %8s %8s %8s", "2", "20", "42", "60", "64\n");
-        System.out.format("%8s %8s %8s %8s %8s", "2", "20", "42", "60", "64\n");
+        System.out.println("HAHAHAHAHAHA");
     }
 }
